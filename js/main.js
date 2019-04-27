292fcf71369a0906aa4b93e4b0a7578936c9e36b
Alexander Dankov <dankunchik@gmail.com>
2019-04-28 00:14:29 +0500

    Вторая часть домашнего задания, урок 5

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let startbtn = document.getElementById("start"),
+    value = document.querySelectorAll(".result-table div:nth-child(even)"),
+    expinput = document.querySelectorAll(".expenses-item"),
+    expbtn = document.getElementsByTagName("button")[0],
+    optexpbtn = document.getElementsByTagName("button")[1],
+    calcbtn = document.getElementsByTagName("button")[2],
+    optexpinput = document.querySelectorAll(".optionalexpenses-item"),
+    incitems = document.querySelector(".choose-income"),
+    chcksavings = document.querySelector(".checksavings"),
+    sumsavings = document.querySelector(".choose-sum"),
+    percsavings = document.querySelector(".choose-percent"),
+    year = document.querySelector(".year"),
+    month = document.querySelector(".month"),
+    day = document.querySelector(".day");
+
+    console.log(startbtn);
+    console.log(value);
+    console.log(expinput);
+    console.log(expbtn);
+    console.log(optexpbtn);
+    console.log(calcbtn);
+    console.log(optexpinput);
+    console.log(incitems);
+    console.log(chcksavings);
+    console.log(sumsavings);
+    console.log(percsavings);
+    console.log(year);
+    console.log(month);
+    console.log(day);
