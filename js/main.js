0f197e28b2bc0e1596536a054984dc3227af1767
Alexander Dankov <dankunchik@gmail.com>
2019-04-29 00:16:38 +0500

    Вторая часть дз (исправление от 28.04)

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
-let startbtn = document.getElementById("start"),
-    value = document.querySelectorAll(".result-table div:nth-child(even)"),
-    expinput = document.querySelectorAll(".expenses-item"),
-    expbtn = document.getElementsByTagName("button")[0],
-    optexpbtn = document.getElementsByTagName("button")[1],
-    calcbtn = document.getElementsByTagName("button")[2],
-    optexpinput = document.querySelectorAll(".optionalexpenses-item"),
-    incitems = document.querySelector(".choose-income"),
-    chcksavings = document.querySelector(".checksavings"),
-    sumsavings = document.querySelector(".choose-sum"),
-    percsavings = document.querySelector(".choose-percent"),
+let startBtn = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName("budget-value")[0],
+    dayBudgetValue = document.getElementsByClassName("daybudget-value")[0],
+    levelValue = document.getElementsByClassName("level-value")[0],
+    expValue = document.getElementsByClassName("expenses-value")[0],
+    optExpValue = document.getElementsByClassName("optionalexpenses-value")[0],
+    incValue = document.getElementsByClassName("income-value")[0],
+    monthSaveValue = document.getElementsByClassName("monthsavings-value")[0],
+    yearSaveValue = document.getElementsByClassName("yearsavings-value")[0],
+    expInput = document.querySelectorAll(".expenses-item"),
+    expBtn = document.getElementsByTagName("button")[0],
+    optExpBtn = document.getElementsByTagName("button")[1],
+    calcBtn = document.getElementsByTagName("button")[2],
+    optExpInput = document.querySelectorAll(".optionalexpenses-item"),
+    incItems = document.querySelector(".choose-income"),
+    chckSavings = document.querySelector(".checksavings"),
+    sumSavings = document.querySelector(".choose-sum"),
+    percSavings = document.querySelector(".choose-percent"),
     year = document.querySelector(".year"),
     month = document.querySelector(".month"),
     day = document.querySelector(".day");
 
-    console.log(startbtn);
-    console.log(value);
-    console.log(expinput);
-    console.log(expbtn);
-    console.log(optexpbtn);
-    console.log(calcbtn);
-    console.log(optexpinput);
-    console.log(incitems);
-    console.log(chcksavings);
-    console.log(sumsavings);
-    console.log(percsavings);
+    console.log(startBtn);
+    console.log(budgetValue);
+    console.log(dayBudgetValue);
+    console.log(levelValue);
+    console.log(expValue);
+    console.log(optExpValue);
+    console.log(incValue);
+    console.log(monthSaveValue);
+    console.log(yearSaveValue);
+    console.log(expInput);
+    console.log(expBtn);
+    console.log(optExpBtn);
+    console.log(calcBtn);
+    console.log(optExpInput);
+    console.log(incItems);
+    console.log(chckSavings);
+    console.log(sumSavings);
+    console.log(percSavings);
     console.log(year);
     console.log(month);
     console.log(day);
